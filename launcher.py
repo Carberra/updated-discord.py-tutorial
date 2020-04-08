bad05fdca600378baf88e36a4fd83432109422af
parafoxia <43846948+parafoxia@users.noreply.github.com>
2020-04-08 15:48:32 +0100

    After episode 7 (forgot to commit episode 6)

--- launcher.py
+++ launcher.py
@@ -1,5 +1,5 @@
 from lib.bot import bot
 
-VERSION = "0.0.1"
+VERSION = "0.0.3"
 
 bot.run(VERSION)
--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -1,5 +1,9 @@
+from datetime import datetime
+
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from discord import Embed, File
 from discord.ext.commands import Bot as BotBase
+from discord.ext.commands import CommandNotFound
 
 PREFIX = "+"
 OWNER_IDS = [385807530913169426]
@@ -17,7 +21,7 @@ class Bot(BotBase):
 	def run(self, version):
 		self.VERSION = version
 
-		with open("./lib/bot/token", "r", encoding="utf-8") as tf:
+		with open("./lib/bot/token.0", "r", encoding="utf-8") as tf:
 			self.TOKEN = tf.read()
 
 		print("running bot...")
@@ -29,10 +33,42 @@ class Bot(BotBase):
 	async def on_disconnect(self):
 		print("bot disconnected")
 
+	async def on_error(self, err, *args, **kwargs):
+		if err == "on_command_error":
+			await args[0].send("Something went wrong.")
+
+		channel = self.get_channel(696812072918581348)
+		await channel.send("An error occured.")
+		raise
+
+	async def on_command_error(self, ctx, exc):
+		if isinstance(exc, CommandNotFound):
+			pass
+
+		else:
+			raise exc.original
+
 	async def on_ready(self):
 		if not self.ready:
 			self.ready = True
+			self.guild = self.get_guild(626608699942764544)
 			print("bot ready")
+
+			channel = self.get_channel(696812072918581348)
+			await channel.send("Now online!")
+
+			embed = Embed(title="Now online!", description="Carberretta is now online.",
+						  colour=0xFF0000, timestamp=datetime.utcnow())
+			fields = [("Name", "Value", True),
+					  ("Another field", "This field is next to the other one.", True),
+					  ("A non-inline field", "This field will appear on it's own row.", False)]
+			for name, value, inline in fields:
+				embed.add_field(name=name, value=value, inline=inline)
+			embed.set_author(name="Carberra Tutorials", icon_url=self.guild.icon_url)
+			embed.set_footer(text="This is a footer!")
+			await channel.send(embed=embed)
+
+			await channel.send(file=File("./data/images/profile.png"))
 
 		else:
 			print("bot reconnected")
