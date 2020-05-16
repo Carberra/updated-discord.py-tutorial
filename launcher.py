c2d71b1dce936eea9e96b87b492ba0c2ba3eb242
parafoxia <43846948+parafoxia@users.noreply.github.com>
2020-05-16 16:52:44 +0100

    After episode 16

--- launcher.py
+++ launcher.py
@@ -1,5 +1,5 @@
 from lib.bot import bot
 
-VERSION = "0.0.11" # Part 15
+VERSION = "0.0.12" # Part 16
 
 bot.run(VERSION)
---
+++ lib/cogs/welcome.py
@@ -0,0 +1,37 @@
+from discord import Forbidden
+from discord.ext.commands import Cog
+from discord.ext.commands import command
+
+from ..db import db
+
+
+class Welcome(Cog):
+	def __init__(self, bot):
+		self.bot = bot
+
+	@Cog.listener()
+	async def on_ready(self):
+		if not self.bot.ready:
+			self.bot.cogs_ready.ready_up("welcome")
+
+	@Cog.listener()
+	async def on_member_join(self, member):
+		db.execute("INSERT INTO exp (UserID) VALUES (?)", member.id)
+		await self.bot.get_channel(711238345224880158).send(f"Welcome to **{member.guild.name}** {member.mention}! Head over to <#626608699942764548> to say hi!")
+
+		try:
+			await member.send(f"Welcome to **{member.guild.name}**! Enjoy your stay!")
+
+		except Forbidden:
+			pass
+
+		await member.add_roles(member.guild.get_role(626609604813651979), member.guild.get_role(626609649294114857))
+
+	@Cog.listener()
+	async def on_member_remove(self, member):
+		db.execute("DELETE FROM exp WHERE UserID = ?", member.id)
+		await self.bot.get_channel(711238360617975829).send(f"{member.display_name} has left {member.guild.name}.")
+
+
+def setup(bot):
+	bot.add_cog(Welcome(bot))
