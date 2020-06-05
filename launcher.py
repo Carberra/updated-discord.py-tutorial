6340d76d26733a7a331d02fa0d838e0016bb5b33
parafoxia <43846948+parafoxia@users.noreply.github.com>
2020-06-05 17:54:39 +0100

    After episode 23

--- launcher.py
+++ launcher.py
@@ -1,5 +1,5 @@
 from lib.bot import bot
 
-VERSION = "0.0.18" # Part 22
+VERSION = "0.0.19" # Part 23
 
 bot.run(VERSION)
--- lib/cogs/mod.py
+++ lib/cogs/mod.py
@@ -2,12 +2,16 @@ from asyncio import sleep
 from datetime import datetime, timedelta
 from typing import Optional
 
+from better_profanity import profanity
 from discord import Embed, Member
 from discord.ext.commands import Cog, Greedy
 from discord.ext.commands import CheckFailure
 from discord.ext.commands import command, has_permissions, bot_has_permissions
 
 from ..db import db
+
+profanity.load_censor_words_from_file("./data/profanity.txt")
+
 
 class Mod(Cog):
 	def __init__(self, bot):
@@ -199,6 +203,27 @@ class Mod(Cog):
 		else:
 			await self.unmute(ctx.guild, targets, reason=reason)
 
+	@command(name="addprofanity", aliases=["addswears", "addcurses"])
+	@has_permissions(manage_guild=True)
+	async def add_profanity(self, ctx, *words):
+		with open("./data/profanity.txt", "a", encoding="utf-8") as f:
+			f.write("".join([f"{w}\n" for w in words]))
+
+		profanity.load_censor_words_from_file("./data/profanity.txt")
+		await ctx.send("Action complete.")
+
+	@command(name="delprofanity", aliases=["delswears", "delcurses"])
+	@has_permissions(manage_guild=True)
+	async def remove_profanity(self, ctx, *words):
+		with open("./data/profanity.txt", "r", encoding="utf-8") as f:
+			stored = [w.strip() for w in f.readlines()]
+
+		with open("./data/profanity.txt", "w", encoding="utf-8") as f:
+			f.write("".join([f"{w}\n" for w in stored if w not in words]))
+
+		profanity.load_censor_words_from_file("./data/profanity.txt")
+		await ctx.send("Action complete.")
+
 	@Cog.listener()
 	async def on_ready(self):
 		if not self.bot.ready:
@@ -220,6 +245,13 @@ class Mod(Cog):
 				await self.unmute(self.bot.guild, unmutes)
 
 			self.bot.cogs_ready.ready_up("mod")
+
+	@Cog.listener()
+	async def on_message(self, message):
+		if not message.author.bot:
+			if profanity.contains_profanity(message.content):
+				await message.delete()
+				await message.channel.send("You can't use that word here.")
 
 
 def setup(bot):
