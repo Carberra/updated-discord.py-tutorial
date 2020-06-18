0dc0af92317933388d8ea08c8a1e970620fe1937
parafoxia <43846948+parafoxia@users.noreply.github.com>
2020-06-18 21:22:32 +0100

    After episode 27

--- launcher.py
+++ launcher.py
@@ -1,5 +1,5 @@
 from lib.bot import bot
 
-VERSION = "0.0.22" # Part 26
+VERSION = "0.0.23" # Part 27
 
 bot.run(VERSION)
--- lib/cogs/reactions.py
+++ lib/cogs/reactions.py
@@ -8,24 +8,38 @@ class Reactions(Cog):
 	@Cog.listener()
 	async def on_ready(self):
 		if not self.bot.ready:
+			self.colours = {
+				"❤️": self.bot.guild.get_role(653940117680947232), # Red
+				"💛": self.bot.guild.get_role(653940192780222515), # Yellow
+				"💚": self.bot.guild.get_role(653940254293622794), # Green
+				"💙": self.bot.guild.get_role(653940277761015809), # Blue
+				"💜": self.bot.guild.get_role(653940305300815882), # Purple
+				"🖤": self.bot.guild.get_role(653940328453373952), # Black
+			}
+			self.reaction_message = await self.bot.get_channel(723257328819896390).fetch_message(723258202090635285)
 			self.bot.cogs_ready.ready_up("reactions")
 
-	@Cog.listener()
-	async def on_reaction_add(self, reaction, user):
-		print(f"{user.display_name} reacted with {reaction.emoji.name}")
+	# @Cog.listener()
+	# async def on_reaction_add(self, reaction, user):
+	# 	print(f"{user.display_name} reacted with {reaction.emoji.name}")
 
-	@Cog.listener()
-	async def on_reaction_remove(self, reaction, user):
-		print(f"{user.display_name} removed their reaction of {reaction.emoji.name}")
+	# @Cog.listener()
+	# async def on_reaction_remove(self, reaction, user):
+	# 	print(f"{user.display_name} removed their reaction of {reaction.emoji.name}")
 
 	@Cog.listener()
 	async def on_raw_reaction_add(self, payload):
-		print(f"[RAW] {payload.member.display_name} reacted with {payload.emoji.name}")
+		if self.bot.ready and payload.message_id == self.reaction_message.id:
+			current_colours = filter(lambda r: r in self.colours.values(), payload.member.roles)
+			await payload.member.remove_roles(*current_colours, reason="Colour role reaction.")
+			await payload.member.add_roles(self.colours[payload.emoji.name], reason="Colour role reaction.")
+			await self.reaction_message.remove_reaction(payload.emoji, payload.member)
 
-	@Cog.listener()
-	async def on_raw_reaction_remove(self, payload):
-		member = self.bot.guild.get_member(payload.user_id)
-		print(f"[RAW] {member.display_name} removed their reaction of {payload.emoji.name}")
+	# @Cog.listener()
+	# async def on_raw_reaction_remove(self, payload):
+	# 	if self.bot.ready and payload.message_id == self.reaction_message.id:
+	# 		member = self.bot.guild.get_member(payload.user_id)
+	# 		await member.remove_roles(self.colours[payload.emoji.name], reason="Colour role reaction.")
 
 
 def setup(bot):
