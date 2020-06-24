c772ee4911d3602811b0451f02a98fda6ffa1372
parafoxia <43846948+parafoxia@users.noreply.github.com>
2020-06-24 17:21:15 +0100

    After episode 29

--- launcher.py
+++ launcher.py
@@ -1,5 +1,5 @@
 from lib.bot import bot
 
-VERSION = "0.0.24" # Part 28
+VERSION = "0.0.25" # Part 29
 
 bot.run(VERSION)
--- lib/cogs/reactions.py
+++ lib/cogs/reactions.py
@@ -1,14 +1,22 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from discord import Embed
 from discord.ext.commands import Cog
+from discord.ext.commands import command, has_permissions
 
 from ..db import db
+
+# Here are all the number emotes.
+# 0⃣ 1️⃣ 2⃣ 3⃣ 4⃣ 5⃣ 6⃣ 7⃣ 8⃣ 9⃣
+
+numbers = ("1️⃣", "2⃣", "3⃣", "4⃣", "5⃣",
+		   "6⃣", "7⃣", "8⃣", "9⃣", "🔟")
 
 
 class Reactions(Cog):
 	def __init__(self, bot):
 		self.bot = bot
+		self.polls = []
 
 	@Cog.listener()
 	async def on_ready(self):
@@ -25,6 +33,42 @@ class Reactions(Cog):
 			self.starboard_channel = self.bot.get_channel(724367069004693591)
 			self.bot.cogs_ready.ready_up("reactions")
 
+	@command(name="createpoll", aliases=["mkpoll"])
+	@has_permissions(manage_guild=True)
+	async def create_poll(self, ctx, hours: int, question: str, *options):
+		if len(options) > 10:
+			await ctx.send("You can only supply a maximum of 10 options.")
+
+		else:
+			embed = Embed(title="Poll",
+						  description=question,
+						  colour=ctx.author.colour,
+						  timestamp=datetime.utcnow())
+
+			fields = [("Options", "\n".join([f"{numbers[idx]} {option}" for idx, option in enumerate(options)]), False),
+					  ("Instructions", "React to cast a vote!", False)]
+
+			for name, value, inline in fields:
+				embed.add_field(name=name, value=value, inline=inline)
+
+			message = await ctx.send(embed=embed)
+
+			for emoji in numbers[:len(options)]:
+				await message.add_reaction(emoji)
+
+			self.polls.append((message.channel.id, message.id))
+
+			self.bot.scheduler.add_job(self.complete_poll, "date", run_date=datetime.now()+timedelta(seconds=hours),
+									   args=[message.channel.id, message.id])
+
+	async def complete_poll(self, channel_id, message_id):
+		message = await self.bot.get_channel(channel_id).fetch_message(message_id)
+
+		most_voted = max(message.reactions, key=lambda r: r.count)
+
+		await message.channel.send(f"The results are in and option {most_voted.emoji} was the most popular with {most_voted.count-1:,} votes!")
+		self.polls.remove((message.channel.id, message.id))
+
 	@Cog.listener()
 	async def on_raw_reaction_add(self, payload):
 		if self.bot.ready and payload.message_id == self.reaction_message.id:
@@ -32,6 +76,15 @@ class Reactions(Cog):
 			await payload.member.remove_roles(*current_colours, reason="Colour role reaction.")
 			await payload.member.add_roles(self.colours[payload.emoji.name], reason="Colour role reaction.")
 			await self.reaction_message.remove_reaction(payload.emoji, payload.member)
+
+		elif payload.message_id in (poll[1] for poll in self.polls):
+			message = await self.bot.get_channel(payload.channel_id).fetch_message(payload.message_id)
+
+			for reaction in message.reactions:
+				if (not payload.member.bot
+					and payload.member in await reaction.users().flatten()
+					and reaction.emoji != payload.emoji.name):
+					await message.remove_reaction(reaction.emoji, payload.member)
 
 		elif payload.emoji.name == "⭐":
 			message = await self.bot.get_channel(payload.channel_id).fetch_message(payload.message_id)
